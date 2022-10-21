95b5b7678a84df9d55cff8595ec5ec277364b391
dbsmandal <dipakmandaljio420@gmail.com>
2022-10-21 21:35:09 +0530

    Craete file compponet for Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter , Routes, Route} from 'react-router-dom';
+import Home from './Component/Home';
+import About from './Component/About';
+import Product from './Component/Product';
+import Contact from './Component/Contact';
+import SingleProduct from './Component/SingleProduct';
+import Cart from './Component/Cart';
+import NotFound from './Component/NotFound';
 
 function App() {
   return (
     <div className="App">
      <h1>React E-Commerce Website</h1>
+     <BrowserRouter>
+     <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/product' element={<Product/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+      <Route path='/singleproduct/:id' element={<SingleProduct/>}/>
+      <Route path='cart' element={<Cart/>} />
+      <Route path='*' element={<NotFound/>} />
+
+
+
+     </Routes>
+     </BrowserRouter>
+
+
     </div>
   );
 }
---
+++ src/Component/Product.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Product = () => {
+  return (
+    <div>
+      Product Page
+    </div>
+  )
+}
+
+export default Product
