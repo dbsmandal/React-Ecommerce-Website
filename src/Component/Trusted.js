1278af600768aa023ea52a40107f556170fcb531
dbsmandal <dipakmandaljio420@gmail.com>
2022-10-24 00:41:44 +0530

    fixed trusted section

--- src/Component/Trusted.js
+++ src/Component/Trusted.js
@@ -2,15 +2,15 @@ import React from 'react'
 
 const Trusted = () => {
   return (
-    <div  className=' h-48  max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-5xl  gap-5 '>
-        <div className='mt-5' >
+    <div  className=' h-48  max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-5xl   '>
+        <div className='grid gap-y-2' >
         <h3 className='text-center font-semibold text-xl'>Trusted By 1000+ Companies</h3>
-        <div className='grid grid-cols-5 gap-5 p-5'>
+        <div className='grid grid-cols-5 gap-3 pl-2 pr-2'>
             <div  className=' grid place-content-center bg-gray-100 rounded-3xl  shadow-md overflow-hidden'>
                 <img src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image2.png"
               alt="trusted-brands" />
             </div>
-            <div  className=' grid place-content-center bg-gray-100 rounded-3xl  shadow-md overflow-hidden'>
+            <div  className=' grid place-content-center bg-gray-100 rounded-3xl   shadow-md overflow-hidden h-auto'>
                 <img src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image3.png"
               alt="trusted-brands" />
             </div>
@@ -25,8 +25,9 @@ const Trusted = () => {
             <div  className=' grid place-content-center bg-gray-100 rounded-3xl  shadow-md overflow-hidden'>
                 <img src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image8.png"
               alt="trusted-brands" />
+             
             </div>
-           
+       
         </div>
         </div>
       
