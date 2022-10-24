acc1bacec4b507ea3793b6a1547957a6de821d26
dbsmandal <dipakmandaljio420@gmail.com>
2022-10-25 00:07:59 +0530

    installled axios for fetch data and structure

--- src/Context/ProdutContext.js
+++ src/Context/ProdutContext.js
@@ -1,9 +1,24 @@
-import { createContext } from "react"
+import { createContext, useEffect } from "react"
+import axios from "axios";
 
 const AppContext=createContext();
+const API='https://api.pujakaitem.com/api/products';
 
 const AppProvider=({children})=>{
-    return<AppContext.Provider value={"dbsmandal"}>{children}</AppContext.Provider>
+//call the api
+    const getProducts=async(url)=>{
+        const res=await axios.get(url);
+        console.log("url data", res)
+        const products= await res.data;
+        console.log('prouct data' , products)
+    }
+    //call api under useefect and for ibe time []
+    useEffect(()=>{
+        getProducts(API)
+    },[])
+    return (  
+          <AppContext.Provider value={"dbsmandal"}>{children}</AppContext.Provider>
+    )
 }
 
 
